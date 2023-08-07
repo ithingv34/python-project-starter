7019393fdcbbf7bdb9f941f00c5a6c516d76c30a
ithingv34 <ithingv34@gmail.com>
2023-08-07 23:49:43 +0000

    fix directory name

--- src/app/main.py
+++ src/app/main.py
@@ -27,4 +27,4 @@ def get_pokemon(
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
